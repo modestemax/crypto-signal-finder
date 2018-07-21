32e350f098b80bd50f7b53b011984561d3c3ac06
max <modestemax@gmail.com>
2018-07-21 03:04:04 +0100

    ..

--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 console.log('Starting App');
 
 const formatError = require('format-error').format;
-
+const {timeframesIntervals}=require('common/settings');
 
 const env = {
     PRODUCTION: !!process.env.PRODUCTION,
     TIMEFRAMES: (process.env.TIMEFRAMES || '1,5,15,60').split(','),
     SYMBOLS_FILTER: process.env.SYMBOLS_FILTER || 'BTC$',
     EXCHANGE: process.env.EXCHANGE || 'binance',
-    timeframesIntervals: {
-        1: 60e3,
-        5: 5 * 60e3,
-        15: 15 * 60e3,
-        60: 60 * 60e3,
-        240: 240 * 60e3,
-        [60 * 24]: 60 * 24 * 60e3,
-    },
+    timeframesIntervals,
 };
 const appEmitter = new class extends (require('events')) {
     constructor() {
